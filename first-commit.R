b99cf76877349d53d5be3082829c90e3901ba65e
ChrisJ78 <cjoubert@gmail.com>
2018-07-04 21:11:35 +0200

    first commit

---
+++ first-commit.R
@@ -0,0 +1,13 @@
+install.packages("ggplot2")
+library(ggplot2)
+
+# create some data
+x <- runif(100)
+# save as RData file
+save(x, file = "randomnumbers.RData")
+
+# some workings
+y <- 2*x 
+mydata <- data.frame(y = y, x = x)
+xyplot <- ggplot(mydata, aes(x = x, y = y)) + geom_point()
+ggsave("xyplot.png", xyplot, width = 7, height = 6, dpi = 200)
